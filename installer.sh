68b586bb023caae001ba399cfbbc99441800c6bd
Gureet Sidhu <gsidh761@mtroyal.ca>
2026-09-14 11:00:45 -0600

    Add installer, bin, libs and etc...

---
+++ installer.sh
@@ -0,0 +1,11 @@
+echo "Setting Up Enviroment..."
+sudo cp -r ../SteemSSELinux/ /usr/local/bin/
+echo "Installing Libs..."
+sudo cp ./libs/* /usr/lib
+echo "Installing XWayland to fix crashes on launch..."
+sudo dnf install xorg-x11-server-Xwayland
+echo "Installing Start Menu Shortcut..."
+sudo cp xsteem64.desktop ~/.local/share/applications
+echo "Done..."
+bash --login
+sudo ldconfig
