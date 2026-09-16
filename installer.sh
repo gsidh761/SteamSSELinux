8058a02a25b87dd2d7d34eba10afb29591e28ae3
Gureet Sidhu <gsidh761@mtroyal.ca>
2026-09-15 19:52:36 -0600

    Init for Ubuntu Port...

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-echo "SteamSSE Linux v1.0.0"
+echo "SteamSSE Linux Port For Ubuntu v1.0.0"
 echo "Made By Gureet Sidhu (@gsidh761)"
 echo ""
 echo "Setting Up Enviroment..."
@@ -6,11 +6,6 @@ rm -rf ~/.local/share/applications/xsteem64.desktop
 sudo cp -r ../SteemSSELinux/ /usr/local/bin/
 echo "Installing Libs..."
 sudo cp ./libs/* /usr/lib
-sudo cp ./libs/* /usr/lib64
-sudo cp ./libs/* /usr/local/lib
-sudo cp ./libs/* /usr/local/lib64
-echo "Installing XWayland to fix crashes on launch..."
-sudo dnf install xorg-x11-server-Xwayland
 echo "Installing Start Menu Shortcut..."
 sudo cp xsteem64.desktop ~/.local/share/applications
 echo "Installing Command Line Utility..."
